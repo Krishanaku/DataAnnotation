efdbca8419ef69bb95c693860ec4d6450fc50e9c
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 12:17:45 +0530

    Add : Data Annotation Examples

---
+++ Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotation
+{
+    public  class Employee
+    {
+        [Required(ErrorMessage ="Employee {0} is required")]
+        [StringLength(100,MinimumLength =3,ErrorMessage ="Name should be minium 3 characters and a maximum of 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Range(18,99,ErrorMessage ="Age Should be above 18")]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
