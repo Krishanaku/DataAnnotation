6c89157d3ef317edbb0ec514c8ed4d42a7a20850
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 12:14:35 +0530

    Add : DataAnnotation and Garbage Collection

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAnnotation
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Data Annotation and Garabge examples");
+
+        }
+    }
+}
