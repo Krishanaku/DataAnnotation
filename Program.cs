dea82f22ca4d197fa1078912e6605384998c4b32
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 12:19:28 +0530

    Add : Garabage Collection examples

--- Program.cs
+++ Program.cs
@@ -5,9 +5,27 @@
         public static void Main(string[] args) 
         {
             // Data Annotation........................
-            AnnotationTest.display();
+            //AnnotationTest.display();
 
-          
+            // Garbage collection examples.............
+
+            long men1 = GC.GetTotalMemory(false);// excuites synchronous manner it will wait until go to the memory and collect
+            {
+                int[] values = new int[5000];
+                values = null;
+
+
+            }
+            long men2 = GC.GetTotalMemory(false);
+            {
+                GC.Collect();
+            }
+            long  men3 = GC.GetTotalMemory(false);
+            {
+                Console.WriteLine(men1);
+                Console.WriteLine(men2);
+                Console.WriteLine(men3);
+            }
 
         }
     }
